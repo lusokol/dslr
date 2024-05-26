7c2ca949e34bc2cc8c1e4599a628b377e2f2a370
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-05-26 16:41:12 +0200

    ajout du l'affichage en scatter

--- srcs/histogram.py
+++ srcs/histogram.py
@@ -1,6 +1,7 @@
 from load_csv import load
 import matplotlib.pyplot as plt
 import pandas as pd
+from tqdm import tqdm
 
 # Gryffindor : red
 # Ravenclaw : blue
@@ -26,41 +27,25 @@ def splitHouses(dataset):
     df_combined = [df_ravenclaw, df_gryffindor, df_hufflepuff, df_slytherin]
     
     return df_combined
-
-    # print(f"{houses[0]}/n")
-    # exit()
-        # print(student)
-        # print(student[1])
-        # exit()
-# Arithmancy,Astronomy,Herbology,Defense Against the Dark Arts,Divination,Muggle Studies,Ancient Runes,History of Magic,Transfiguration,Potions,Care of Magical Creatures,Charms,Flying
-# 13 matieres
 def main():
     dataset = load("../datasets/dataset_train.csv")
-    # data = dataset["Arithmancy"].values
     data = splitHouses(dataset.values)
-    # print(data[0].columns)
-    # exit()
-    figure, axis = plt.subplots(4, 4) 
+    figure, axis = plt.subplots(4, 4, figsize=(30, 30)) 
     i = 0
-    for idx, col in enumerate(data[0].columns[6:]):
+    for idx, col in tqdm(enumerate(data[0].columns[6:]), total=13, desc=f'Loading histograms', position=0, leave=True):
         x = int(idx % 4)
         y = int(idx / 4)
-        # print(data[0]["Arithmancy"])
-        # print(f"x = {x}, y = {y}")
-        # exit()
-        print(col)
         axis[y, x].hist(data[0][col], color="blue", alpha=0.3, edgecolor='black', label="Ravenclaw")
         axis[y, x].hist(data[1][col], color="red", alpha=0.3, edgecolor='black',label="Gryffindor")
         axis[y, x].hist(data[2][col], color="yellow", alpha=0.3, edgecolor='black',label="Hufflepuff")
         axis[y, x].hist(data[3][col], color="green", alpha=0.3, edgecolor='black',label="Slytherin")
         axis[y, x].set_title(col)
-        axis[y, x].legend()
         i = idx + 1
     while i < 16:
         figure.delaxes(axis[int(i / 4), int(i % 4)])
         i += 1        
-    # plt.legend()
-    plt.show()
+    plt.legend(loc='lower right', frameon=False, bbox_to_anchor=(4.5,0), prop={'size': 30})
+    plt.savefig('../img/histograms.png', dpi=300, bbox_inches='tight')
 
 if __name__ == "__main__":
     main()
